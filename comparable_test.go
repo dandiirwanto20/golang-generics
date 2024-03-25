f136bd261a83ea188029caf041fb4a621f340fa2
Dandi Irwanto <dandi.ir20@gmail.com>
2024-03-25 15:16:28 +0700

    learn: multiple type param generic

---
+++ comparable_test.go
@@ -0,0 +1,20 @@
+package gogeneric
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func IsSame[T comparable](value1, value2 T) bool {
+	if value1 == value2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func TestIsSame(t *testing.T) {
+	assert.True(t, IsSame[string]("Dandi", "Dandi"))
+	assert.False(t, IsSame[int](10, 100))
+}
